810f90ccb0d12457fe5ba6af0b3c0df98dbb3113
Falase Femi <femifalase228@gmail.com>
2023-10-16 22:13:17 +0100

    rewriting the   navbar

--- project1/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
-    const navLinks = document.querySelector(".nav-links");
-
-    mobileMenuIcon.addEventListener("click", function () {
-        navLinks.classList.toggle("mobile-visible");
-        mobileMenuIcon.classList.toggle("open");
-    });
-});
